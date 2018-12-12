2c89e118344597d8bac70ab7867aa30f1c242703
SeonghwanKim13109 <reasonby13@naver.com>
2018-12-12 19:34:30 +0900

    use variable and print string

---
+++ variable.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Variables</h1>
+   <?php
+    $var = "Seonghwan";
+      echo "Lorem ipsum dolor sit amet,".$var."consectetur adipisicing
+      elit, sed do eiusmod tempor incididunt ut labore et
+      dolore magna aliqua.
+      Ut enim ad minim veniam, ".$var."quis nostrud exercitation
+      ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      Duis aute irure dolor in ".$var."reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur.
+      Excepteur sint occaecat cupidatat non proident,
+      sunt in culpa qui ".$var."officia deserunt mollit anim
+      id est laborum.";
+     ?>
+  </body>
+</html>

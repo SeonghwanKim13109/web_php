3251323b65bd33c874106d1bf2b2dd47fa006335
SeonghwanKim13109 <reasonby13@naver.com>
2018-12-17 23:04:32 +0900

    read directory and use loop to make list automatically

--- index.php
+++ index.php
@@ -6,11 +6,27 @@
  </head>
  <body>
    <h1><a href="index.php">WEB</a></h1>
-   <ol>
+   <!-- list는 목차가 추가될 때마다 수동으로 바껴야 하는 부분 -->
+   <!-- <ol>
      <li><a href="index.php?id=HTML">HTML</a></li>
      <li><a href="index.php?id=CSS">CSS</a></li>
      <li><a href="index.php?id=JavaScript">JavaScript</a></li>
-   </ol>
+     <li><a href="index.php?id=PHP">PHP</a></li>
+   </ol> -->
+   <!--
+    data 디렉토리에 있는 파일의 목록을 가져온다
+    파일의 목록의 각 개체 마다 li 와 a 태그를 이용하여 목록을 추가한다
+    -->
+    <?php
+    $list = scandir('data');
+    $i = 0;
+    while($i <count($list)){
+      if($list[$i] !='.' && $list[$i] != '..'){
+          echo '<li><a href = "index.php?id='.$list[$i].'">'.$list[$i].'</a></li>';
+        }
+      $i++;
+    }
+    ?>
    <h2>
    <?php
      if(isset($_GET['id'])){
